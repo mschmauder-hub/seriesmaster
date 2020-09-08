e637b85445a657421612728c402ac706a5b11e8c
mschmauder-hub <marius.schmauder.ms@gmail.com>
2020-09-08 16:40:12 +0200

    Add Header component and prop-type validation

--- SeriesMaster-Frontend/App.js
+++ SeriesMaster-Frontend/App.js
@@ -1,21 +1,25 @@
 import React from "react";
 import styled from "styled-components/native";
+import { StatusBar } from "react-native";
+import Header from "./components/Header";
 
 const Container = styled.View`
-  height: 100%;
-  display: flex;
+  flex: 1;
   align-items: center;
-  justify-content: center;
+  background: #20232a;
+  padding-top: ${StatusBar.currentHeight};
 `;
 
 const StyledText = styled.Text`
   color: red;
+  align-items: center;
+  background: green;
 `;
 
 export default function App() {
-  console.log("Test");
   return (
     <Container>
+      <Header title="Discover"></Header>
       <StyledText>Open up your app! Test</StyledText>
     </Container>
   );
---
+++ SeriesMaster-Frontend/components/Header.js
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components/native";
+import PropTypes from "prop-types";
+
+const HeaderStyle = styled.View`
+  display: flex;
+  flex-direction: row;
+  padding: 10px;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  border-width: 1px;
+  border-bottom-color: #707070;
+`;
+
+const StyledText = styled.Text`
+  margin: auto;
+  padding-left: 30px;
+  color: #fff;
+  font-size: 16px;
+`;
+
+const StyledImage = styled.Image`
+  height: 30px;
+  width: 30px;
+`;
+
+const Header = ({ title }) => {
+  return (
+    <HeaderStyle>
+      <StyledText>{title}</StyledText>
+      <StyledImage source={require("../assets/menu-white.svg")} />
+    </HeaderStyle>
+  );
+};
+
+export default Header;
+
+Header.propTypes = {
+  title: PropTypes.any,
+};
