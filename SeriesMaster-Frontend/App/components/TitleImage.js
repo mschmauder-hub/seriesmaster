cc3a8310699f9fb15b605d9132aaed2369aac378
mschmauder-hub <marius.schmauder.ms@gmail.com>
2020-09-10 13:47:03 +0200

    Rename title image styled component

--- SeriesMaster-Frontend/App/components/TitleImage.js
+++ SeriesMaster-Frontend/App/components/TitleImage.js
@@ -2,14 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components/native";
 
-const Image2 = styled.Image`
+const Image = styled.Image`
   height: 200px;
   width: 200px;
   margin: 20px 0 10px 0;
 `;
 
 const TitleImage = ({ imageSrc }) => {
-  return <Image2 source={imageSrc} />;
+  return <Image source={imageSrc} />;
 };
 
 export default TitleImage;
