f35141760d1ec14f2cf4bedf705589a34ca5ee87
mschmauder-hub <marius.schmauder.ms@gmail.com>
2020-09-16 14:12:39 +0200

    Basic mongodb setup and user router

---
+++ SeriesMaster-Backend/routes/users.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+function createUserRouter(database) {
+  const router = express.Router();
+
+  router.get("/", async (req, res) => {
+    const collection = database.collection("users");
+    const user = await collection.findOne({
+      user: "data",
+    });
+    console.log(user);
+    res.send(user);
+  });
+
+  return router;
+}
+
+module.exports = createUserRouter;
