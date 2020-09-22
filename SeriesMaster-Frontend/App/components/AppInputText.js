818b5b70bf3cba24e955cff9ddc95a8296c593e5
mschmauder-hub <marius.schmauder.ms@gmail.com>
2020-09-22 15:17:44 +0200

    Add icon and change input component size

--- SeriesMaster-Frontend/App/components/AppInputText.js
+++ SeriesMaster-Frontend/App/components/AppInputText.js
@@ -1,13 +1,17 @@
 import React from "react";
 import styled from "styled-components/native";
 import PropTypes from "prop-types";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import colors from "../config/colors";
 
 const InputContainer = styled.View`
   background: #fcfcfc;
   border-radius: 25px;
-  margin: 10px;
-  width: 50%;
+  margin: 5px;
+  width: 70%;
   align-self: center;
+  flex-direction: row;
+  align-items: center;
 `;
 
 const InputText = styled.TextInput`
@@ -16,11 +20,17 @@ const InputText = styled.TextInput`
   margin-left: 5px;
 `;
 
-const AppInputText = ({ placeholder, query, onChange }) => {
+const Icon = styled(MaterialCommunityIcons)`
+  margin-left: 10px;
+  color: ${colors.grey};
+`;
+
+const AppInputText = ({ icon, iconSize, placeholder, input, onChange }) => {
   return (
     <InputContainer>
+      {icon && <Icon name={icon} size={iconSize} />}
       <InputText
-        value={query}
+        value={input}
         onChangeText={onChange}
         placeholder={placeholder}
       />
@@ -29,9 +39,11 @@ const AppInputText = ({ placeholder, query, onChange }) => {
 };
 
 AppInputText.propTypes = {
-  query: PropTypes.string,
+  input: PropTypes.string,
   onChange: PropTypes.func,
   placeholder: PropTypes.string,
+  icon: PropTypes.string,
+  iconSize: PropTypes.number,
 };
 
 export default AppInputText;
