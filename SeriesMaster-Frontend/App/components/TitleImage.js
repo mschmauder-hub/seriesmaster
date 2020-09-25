e7f2b88ad822d5983ead20560799b2601cf0bdfb
mschmauder-hub <marius.schmauder.ms@gmail.com>
2020-09-25 12:59:00 +0200

    Adjust styling details screen and add loading screen

--- SeriesMaster-Frontend/App/components/TitleImage.js
+++ SeriesMaster-Frontend/App/components/TitleImage.js
@@ -3,8 +3,8 @@ import PropTypes from "prop-types";
 import styled from "styled-components/native";
 
 const Image = styled.Image`
-  height: 200px;
-  width: 200px;
+  height: 400px;
+  width: 80%;
   margin: 20px 0 10px 0;
 `;
 
