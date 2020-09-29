78dc1c5ef14bb2cbc2c0920dc934820d1f4e6360
mschmauder-hub <marius.schmauder.ms@gmail.com>
2020-09-29 11:37:49 +0200

    Add cron job to execute code regularly

---
+++ SeriesMaster-Backend/lib/jobs.js
@@ -0,0 +1,19 @@
+const CronJob = require("cron").CronJob;
+const { checkApi, checkUpdates } = require("./checkUpdates");
+
+function runUpdateShows(database) {
+  const job = new CronJob("*/1 * * * *", async () => {
+    const shows = await checkApi(database);
+
+    const allShows = await Promise.all(
+      shows.map((show) => checkUpdates(database, show))
+    );
+
+    const updatedShows = allShows.filter((myShow) => myShow);
+    console.log(updatedShows);
+  });
+
+  job.start();
+}
+
+module.exports = { runUpdateShows };
--- SeriesMaster-Backend/server.js
+++ SeriesMaster-Backend/server.js
@@ -1,17 +1,18 @@
 require("dotenv").config();
 const express = require("express");
-
-const app = express();
+const bodyParser = require("body-parser");
 const { MongoClient } = require("mongodb");
+
 const createAuthRouter = require("./routes/auth");
 const createShowsRouter = require("./routes/shows");
 const createUserRouter = require("./routes/users");
+const { runUpdateShows } = require("./lib/jobs");
 
-const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 3001;
 
-app.use(bodyParser.json());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -26,11 +27,9 @@ const client = new MongoClient(process.env.MONGO_URI, {
 });
 
 async function main() {
+  await client.connect();
+  const database = client.db(process.env.MONGO_NAME);
   try {
-    await client.connect();
-
-    const database = client.db(process.env.MONGO_NAME);
-
     app.use("/api/shows", createShowsRouter(database));
     app.use("/api/users", createUserRouter(database));
     app.use("/api/auth", createAuthRouter(database));
@@ -38,6 +37,8 @@ async function main() {
     app.listen(port, () => {
       console.log("listening on 3001");
     });
+
+    runUpdateShows(database);
   } catch (error) {
     console.log(error);
   }
