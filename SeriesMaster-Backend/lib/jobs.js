2de62c389e42dfbbcd627fa47f5f5090a533f266
mschmauder-hub <marius.schmauder.ms@gmail.com>
2020-09-30 17:30:55 +0200

    Add function to send notifications to the user when a show updates

--- SeriesMaster-Backend/lib/jobs.js
+++ SeriesMaster-Backend/lib/jobs.js
@@ -1,8 +1,11 @@
 const CronJob = require("cron").CronJob;
 const { checkApi, checkUpdates } = require("./checkUpdates");
+const { Expo } = require("expo-server-sdk");
+const { checkUsersForShow } = require("./checkUsersforShow");
 
 function runUpdateShows(database) {
-  const job = new CronJob("*/10 * * * *", async () => {
+  const job = new CronJob("*/30 * * * *", async () => {
+    let expo = new Expo();
     const shows = await checkApi(database);
 
     const allShows = await Promise.all(
@@ -10,7 +13,27 @@ function runUpdateShows(database) {
     );
 
     const updatedShows = allShows.filter((myShow) => myShow);
-    console.log(updatedShows);
+
+    if (updatedShows.length < 1) {
+      return;
+    }
+
+    const allMessages = await Promise.all(
+      updatedShows.map(async (show) => {
+        const users = await checkUsersForShow(database, show);
+
+        return users.map((user) => ({
+          to: user.token,
+          sound: "default",
+          body: `The following show has been updated: ${show.title}`,
+        }));
+      })
+    );
+
+    allMessages.forEach(async (messages) => {
+      const ticket = await expo.sendPushNotificationsAsync(messages);
+      console.log(ticket);
+    });
   });
 
   job.start();
